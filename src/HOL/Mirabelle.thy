2360749b3bd62b01535afc1b83be03b1ae9ac663
Felix Schnabel <f.schnabel@tum.de>
2024-04-05 01:02:14 +0200

    Apply initial Proof_Improve

--- src/HOL/Mirabelle.thy
+++ src/HOL/Mirabelle.thy
@@ -6,7 +6,7 @@
 *)
 
 theory Mirabelle
-  imports Sledgehammer Predicate_Compile Presburger
+  imports Sledgehammer Predicate_Compile Presburger Proof_Improve
 begin
 
 ML_file \<open>Tools/Mirabelle/mirabelle_util.ML\<close>
@@ -21,6 +21,7 @@ end
 ML_file \<open>Tools/Mirabelle/mirabelle_arith.ML\<close>
 ML_file \<open>Tools/Mirabelle/mirabelle_metis.ML\<close>
 ML_file \<open>Tools/Mirabelle/mirabelle_presburger.ML\<close>
+ML_file \<open>Tools/Mirabelle/mirabelle_proof_improve.ML\<close>
 ML_file \<open>Tools/Mirabelle/mirabelle_quickcheck.ML\<close>
 ML_file \<open>Tools/Mirabelle/mirabelle_sledgehammer_filter.ML\<close>
 ML_file \<open>Tools/Mirabelle/mirabelle_sledgehammer.ML\<close>
---
+++ src/HOL/Proof_Improve.thy
@@ -0,0 +1,18 @@
+(*  Title:      HOL/Proof_Improve.thy
+    Author:     Daniel Lipkin, TU Muenchen
+*)
+
+section \<open>Proof Improve: Semi-Automatic Proof Improvement\<close>
+
+theory Proof_Improve
+  imports Sledgehammer
+begin
+
+
+ML_file \<open>Tools/Proof_Improve/proof_improve_changes.ML\<close>
+ML_file \<open>Tools/Proof_Improve/proof_improve_config_manager.ML\<close>
+ML_file \<open>Tools/Proof_Improve/proof_improve_finder.ML\<close>
+ML_file \<open>Tools/Proof_Improve/proof_improve_scorer.ML\<close>
+ML_file \<open>Tools/Proof_Improve/proof_improve.ML\<close>
+
+end
